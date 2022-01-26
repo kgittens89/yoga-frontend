55216f4af019b66be22930fe5278598772795651
pmknight <myattepa@gmail.com>
2022-01-26 15:47:36 -0500

    poseDetails layout

---
+++ src/components/PosesMain/PoseDetails/PoseDetails.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+
+import { useParams } from 'react-router-dom';
+// import axios from 'axios';
+
+import seeds from '../../../seeds.json';
+
+function PoseDetails(props) {
+	const [pose, setPose] = useState(seeds[0]);
+	const { id } = useParams();
+
+	// useEffect(() => {
+	// 	getPoses();
+	// 	console.log(getPoses);
+	// }, [id]);
+
+	// async function getPoses() {
+	// 	try {
+	// 		const res = await fetch(`http://localhost:3000/flowfactory/${id}`);
+	// 		const data = await res.json();
+
+	// 		setPose(data);
+	// 	} catch (error) {
+	// 		console.log(error);
+	// 	}
+	// }
+
+	return (
+		<div>
+			<h2>{pose.englishName}</h2>
+			<img src={pose.image} alt={pose.englishName} />
+			<div className='details'>
+				<p>{pose.sanskritName}</p>
+				<p>{pose.difficulty}</p>
+				<p>{pose.discription}</p>
+				<p>{pose.categories[0].catName}</p>
+				<p>{pose.categories[0].catDescription}</p>
+			</div>
+		</div>
+	);
+}
+
+export default PoseDetails;
