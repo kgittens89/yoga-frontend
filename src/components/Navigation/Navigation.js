a9c6cd4594f079d7147efe7c2634656e22346b45
Keisha Gittens <keishamsa@me.com>
2022-01-26 17:39:57 -0600

    Add links to Navbar

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { MdClose } from 'react-icons/md'
 import { FiMenu } from 'react-icons/fi'
@@ -12,16 +12,37 @@ function Navigation(props) {
         setNavbarOpen(!navbarOpen)
     }
     return (
-        <nav className='navBar'>
-            <button onClick={handleToggle}>{navbarOpen ? (<MdClose style={{ color: '#fff', width: '40px', height: '40px '}} /> ) : (<FiMenu style={{ color: "#7b7b7b", width: "40px", height: "40px" }} />)}</button>
-            <ul className={`menuNav ${navbarOpen ? "showMenu" : ""}`}>
-                <li>All Poses</li>
-                <li>My Sequences</li>
-                <li>Pose of the Day</li>
-                <li>About</li>
-            </ul>
-        </nav>
-    );
+			<nav className='navBar'>
+				<button onClick={handleToggle}>
+					{navbarOpen ? (
+						<MdClose
+							style={{ color: '#fff', width: '40px', height: '40px ' }}
+						/>
+					) : (
+						<FiMenu
+							style={{ color: '#7b7b7b', width: '40px', height: '40px' }}
+						/>
+					)}
+				</button>
+				<ul className={`menuNav ${navbarOpen ? 'showMenu' : ''}`}>
+					<li>
+						<Link to='/'>Home</Link>
+					</li>
+					<li>
+						<Link to='/poses'>All Poses</Link>
+					</li>
+					<li>
+						<Link to='/mysequence'>My Sequences</Link>
+					</li>
+					<li>
+						<Link to='/poseDetails'>Pose of the Day</Link>
+					</li>
+					<li>
+						<Link to='/about'>About</Link>
+					</li>
+				</ul>
+			</nav>
+		);
 }
 
 export default Navigation;
