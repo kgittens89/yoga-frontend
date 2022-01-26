1a7ac1c2722608b45d79e9bea35e4950eaef1214
Keisha Gittens <keishamsa@me.com>
2022-01-26 15:52:17 -0600

    Add deployed api link to My sequence display

--- src/components/MySequenceDisplay/MySequenceDisplay.js
+++ src/components/MySequenceDisplay/MySequenceDisplay.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
 function MySequenceDisplay(props) {
+    const [sequences, setSequences] = useState([]);
+    useEffect(() => {
+        let url = 'https://still-sands-89510.herokuapp.com/flowfactory/sequence';
+        
+        fetch(url)
+        .then(res => res.json())
+        .then((res) => {
+            setSequences(res)
+        })
+    }, [])
+    
     return (
         <div>
-            My Sequence Display
-        </div>
-    );
-}
-
-export default MySequenceDisplay;
+        {sequences.map((sequence) => {
+            return (
+                <>
+                <h4>{sequence.sequenceName}</h4>
+                {sequence.sequencePoses.map((pose) => {
+                    return (
+                        <p key={pose.id}>{pose.englishName}</p>
+                        );
+                    })}
+                    </>
+                    )
+                })}
+                </div>
+                );
+            };
+            
+            export default MySequenceDisplay;
