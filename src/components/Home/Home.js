1084c50102c797d74d95163315608243a83541f0
Keisha Gittens <keishamsa@me.com>
2022-01-27 11:58:32 -0600

    Add custom hook to generate random pose and applied to links in Navigation & Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './Home.css';
 import { Link } from 'react-router-dom';
+import useFetchRandomPose from '../../hooks/useFetchRandomPose';
 
 function Home() {
+    const idForRandomPose = useFetchRandomPose();
+
     return (
         <section>
             <h1 className='title'>The Flow Factory</h1>
@@ -14,7 +17,8 @@ function Home() {
                     <Link to='/mySequence'>My Sequences</Link>
                 </li>
                 <li>
-                    <Link to='/poseDetails'>Pose of the Day</Link>
+                    <Link to={`/poseDetails/${idForRandomPose}`}>Pose of the Day</Link> 
+                    {/* Pose of the Day only works on initial click, Not sure why?? */}
                 </li>
                 <li>
                     <Link to='/about'>About</Link>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,10 +2,12 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { MdClose } from 'react-icons/md';
 import { FiMenu } from 'react-icons/fi';
+import useFetchRandomPose from '../../hooks/useFetchRandomPose';
 // https://react-icons.github.io/react-icons/
 
 function Navigation(props) {
 	const [navbarOpen, setNavbarOpen] = useState(false);
+	  const idForRandomPose = useFetchRandomPose();
 
 	const handleToggle = () => {
 		setNavbarOpen(!navbarOpen);
@@ -30,7 +32,8 @@ function Navigation(props) {
 					<Link to='/mysequence'>My Sequences</Link>
 				</li>
 				<li>
-					<Link to='/poseDetails/:id'>Pose of the Day</Link>
+					<Link to={`/poseDetails/${idForRandomPose}`}>Pose of the Day</Link>
+					{/* Again Pose of the day is acting a little weird. Doesn't always switch out. */}
 				</li>
 				<li>
 					<Link to='/about'>About</Link>
