bfca1aef0294a717ed0955fdd9a1b9815fd84e92
melissa-morgan <melissamorganc@gmail.com>
2022-01-27 10:57:52 -0500

    fixed display on hamburger nav

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,48 +1,43 @@
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
-import { MdClose } from 'react-icons/md'
-import { FiMenu } from 'react-icons/fi'
+import { MdClose } from 'react-icons/md';
+import { FiMenu } from 'react-icons/fi';
 // https://react-icons.github.io/react-icons/
 
 function Navigation(props) {
+	const [navbarOpen, setNavbarOpen] = useState(false);
 
-    const [navbarOpen, setNavbarOpen] = useState(false);
-
-    const handleToggle = () => {
-        setNavbarOpen(!navbarOpen)
-    }
-    return (
-			<nav className='navBar'>
-				<button onClick={handleToggle}>
-					{navbarOpen ? (
-						<MdClose
-							style={{ color: '#fff', width: '40px', height: '40px ' }}
-						/>
-					) : (
-						<FiMenu
-							style={{ color: '#7b7b7b', width: '40px', height: '40px' }}
-						/>
-					)}
-				</button>
-				<ul className={`menuNav ${navbarOpen ? 'showMenu' : ''}`}>
-					<li>
-						<Link to='/'>Home</Link>
-					</li>
-					<li>
-						<Link to='/posesMain'>All Poses</Link>
-					</li>
-					<li>
-						<Link to='/mysequence'>My Sequences</Link>
-					</li>
-					<li>
-						<Link to='/poseDetails/:id'>Pose of the Day</Link>
-					</li>
-					<li>
-						<Link to='/about'>About</Link>
-					</li>
-				</ul>
-			</nav>
-		);
+	const handleToggle = () => {
+		setNavbarOpen(!navbarOpen);
+	};
+	return (
+		<nav className='navBar'>
+			<button onClick={handleToggle}>
+				{navbarOpen ? (
+					<MdClose style={{ color: '#fff', width: '40px', height: '40px ' }} />
+				) : (
+					<FiMenu style={{ color: '#7b7b7b', width: '40px', height: '40px' }} />
+				)}
+			</button>
+			<ul style={{ display: navbarOpen ? 'flex' : 'none' }} className='menuNav'>
+				<li>
+					<Link to='/'>Home</Link>
+				</li>
+				<li>
+					<Link to='/posesMain'>All Poses</Link>
+				</li>
+				<li>
+					<Link to='/mysequence'>My Sequences</Link>
+				</li>
+				<li>
+					<Link to='/poseDetails/:id'>Pose of the Day</Link>
+				</li>
+				<li>
+					<Link to='/about'>About</Link>
+				</li>
+			</ul>
+		</nav>
+	);
 }
 
 export default Navigation;
