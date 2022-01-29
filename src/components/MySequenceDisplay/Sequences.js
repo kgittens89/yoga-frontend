d1117dc8ae156ff0e3909e5fa1fa3d484b1360d5
melissa-morgan <melissamorganc@gmail.com>
2022-01-29 14:15:19 -0500

    collapsible poses on My Sequences

--- src/components/MySequenceDisplay/Sequences.js
+++ src/components/MySequenceDisplay/Sequences.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './MySequenceDisplay.css';
-import {AiOutlineCloseCircle} from 'react-icons/ai'
+import { AiOutlineCloseCircle } from 'react-icons/ai';
+import useCollapse from 'react-collapsed';
 
 function SequenceDetails({
 	sequence,
@@ -10,31 +11,49 @@ function SequenceDetails({
 	editToggle,
 	handleEditClick,
 	deleteClick,
-}) {
+}) 
+{
+		const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
+
 	return (
 		<div className='userSequence'>
 			<Link className='sequenceName' to={`/sequenceDetails/${sequence._id}`}>
 				{' '}
 				<h4>{sequence.sequenceName}</h4>
 			</Link>
-			{sequence.sequencePoses.map((pose) => {
-				return (
-					<div className='imageEdit'>
-						<p key={pose.id}>{pose.englishName}</p>
-						{/* <img
-							src={pose.image}
-							alt={pose.englishName}
-							className='imageSequence'
-						/> */}
-						{index === currentEdit && editToggle ? (
-							<button onClick={() => deleteClick(pose)}><AiOutlineCloseCircle size={20}/></button>
-						) : (
-							''
-						)}
+			<div className='collapsible'>
+				<div className='collapsibleHeader' {...getToggleProps()}>
+					{isExpanded ? 'Hide Poses' : 'See Poses'}
+				</div>
+				<div {...getCollapseProps()}>
+					<div className='content'>
+						{sequence.sequencePoses.map((pose) => {
+							return (
+								<div className='imageEdit'>
+									<p key={pose.id}>{pose.englishName}</p>
+									{/* <img
+											src={pose.image}
+											alt={pose.englishName}
+											className='imageSequence'
+										/> */}
+									{index === currentEdit && editToggle ? (
+										<button onClick={() => deleteClick(pose)}>
+											<AiOutlineCloseCircle size={20} />
+										</button>
+									) : (
+										''
+									)}
+								</div>
+							);
+						})}
+						<button
+							className='editBtn'
+							onClick={() => handleEditClick(sequence, index)}>
+							Edit
+						</button>
 					</div>
-				);
-			})}
-			<button className='editBtn' onClick={() => handleEditClick(sequence, index)}>Edit</button>
+				</div>
+			</div>
 		</div>
 	);
 }
