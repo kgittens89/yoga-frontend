5a2a2f77b0db90c704233086eb5b819ce3329a1b
pmknight <myattepa@gmail.com>
2022-01-29 15:44:47 -0500

    styling edits

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { MdClose } from 'react-icons/md';
 import { FiMenu } from 'react-icons/fi';
-import DarkYogaGirl from '../../Assets/DarkYogaGirl.png'
+import DarkYogaGirl from '../../Assets/DarkYogaGirl.png';
 import useFetchRandomPose from '../../hooks/useFetchRandomPose';
 // https://react-icons.github.io/react-icons/
 
@@ -13,37 +13,45 @@ function Navigation(props) {
 	const handleToggle = () => {
 		setNavbarOpen(!navbarOpen);
 	};
-	
+
 	return (
 		<nav className='navBar'>
 			<div className='header-box'>
 				<h1 className='header-text'>The Flow Factory</h1>
 				<img className='yogo-logo' src={DarkYogaGirl} alt='Logo' />
 			</div>
-			<button onClick={handleToggle}>
-				{navbarOpen ? (
-					<MdClose style={{ color: '#fff', width: '40px', height: '40px ' }} />
-				) : (
-					<FiMenu style={{ color: '#7b7b7b', width: '40px', height: '40px' }} />
-				)}
-			</button>
-			<ul style={{ display: navbarOpen ? 'flex' : 'none' }} className='menuNav'>
-				<li>
-					<Link to='/'>Home</Link>
-				</li>
-				<li>
-					<Link to='/posesMain'>All Poses</Link>
-				</li>
-				<li>
-					<Link to='/mysequence'>My Sequences</Link>
-				</li>
-				<li>
-					<Link to={`/poseDetails/${idForRandomPose}`}>Random Pose</Link>
-				</li>
-				<li>
-					<Link to='/about'>About</Link>
-				</li>
-			</ul>
+			<div>
+				<button onClick={handleToggle}>
+					{navbarOpen ? (
+						<MdClose
+							style={{ color: '#fff', width: '40px', height: '40px ' }}
+						/>
+					) : (
+						<FiMenu
+							style={{ color: '#7b7b7b', width: '40px', height: '40px' }}
+						/>
+					)}
+				</button>
+				<ul
+					style={{ display: navbarOpen ? 'flex' : 'none' }}
+					className='menuNav'>
+					<li>
+						<Link to='/'>Home</Link>
+					</li>
+					<li>
+						<Link to='/posesMain'>All Poses</Link>
+					</li>
+					<li>
+						<Link to='/mysequence'>My Sequences</Link>
+					</li>
+					<li>
+						<Link to={`/poseDetails/${idForRandomPose}`}>Random Pose</Link>
+					</li>
+					<li>
+						<Link to='/about'>About</Link>
+					</li>
+				</ul>
+			</div>
 		</nav>
 	);
 }
