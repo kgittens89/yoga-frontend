35180608c5d7eaa15605a5b03461457f11555421
Keisha Gittens <keishamsa@me.com>
2022-01-29 12:31:28 -0600

    Start of logic to update individual sequence. Add edit functionality. Still need to add update request to backend

--- src/components/MySequenceDisplay/SequenceDetails.js
+++ src/components/MySequenceDisplay/SequenceDetails.js
@@ -4,41 +4,95 @@ import { useParams } from 'react-router-dom';
 import Navigation from '../Navigation/Navigation';
 
 function SequenceDetails(props) {
-	const [sequence, setSequence] = useState(null);
-	const { sequenceId } = useParams();
-
-	useEffect(() => {
-		const url = `https://still-sands-89510.herokuapp.com/flowfactory/sequence/sequenceDetails/${sequenceId}`;
-
-		fetch(url)
-			.then((res) => res.json())
-			.then((res) => {
-				setSequence(res);
-			});
-		//eslint-disable-next-line
-	}, []);
-
-	if (!sequence) {
-		return <p>Loading...</p>;
-	}
-	return (
-		<div>
-            <Navigation />
-			<h2>{sequence.sequenceName}</h2>
-			{sequence.sequencePoses.map((pose) => {
-				return (
-					<div className='poseList' key={pose._id}>
-						<p>{pose.englishName}</p>
-						<img
-							src={pose.image}
-							alt={pose.englishName}
-							className='image-sequence'
-						/>
-					</div>
-				);
-			})}
-		</div>
-	);
-}
-
-export default SequenceDetails;
+    const [editToggle, setEditToggle] = useState(false);
+    const [sequence, setSequence] = useState(null);
+    const { sequenceId } = useParams();
+    
+    useEffect(() => {
+        // const url = `https://still-sands-89510.herokuapp.com/flowfactory/sequence/sequenceDetails/${sequenceId}`;
+        
+        // fetch(url)
+        // .then((res) => res.json())
+        // .then((res) => {
+        //     setSequence(res);
+        // });
+        getSequence()
+        //eslint-disable-next-line
+    }, [sequence]);
+    
+    const getSequence = () => {
+        const url = `https://still-sands-89510.herokuapp.com/flowfactory/sequence/sequenceDetails/${sequenceId}`;
+        
+        fetch(url)
+        .then((res) => res.json())
+        .then((res) => {
+            setSequence(res);
+        });
+    }
+    
+    const handleEditClick = () => {
+        let tempSeq = { ...sequence }
+        console.log(tempSeq)
+        setEditToggle(!editToggle);
+    };
+    
+    const handleChange = (e) => {
+        setSequence({ ...sequence, sequenceName: e.target.value });
+    }
+    
+    const deleteClick = (pose) => {
+        console.log(pose)
+        let tempArr = [...sequence.sequencePoses];
+        console.log(tempArr)
+        // let tempArrPoses = tempArr[0].sequencePoses;
+        
+        let idxToDelete = tempArr.findIndex((temp) => {
+            return temp._id === pose._id;
+        });
+        console.log(idxToDelete)
+        tempArr.splice(idxToDelete, 1);
+        console.log(tempArr);
+        // axios
+        // 	.put(
+        // 		`https://still-sands-89510.herokuapp.com/flowfactory/sequence/pose/${tempArr[0]._id}/${pose._id}`
+        // 	)
+        // 	.then((res) => console.log(res))
+        // 	.catch((err) => console.log(err));
+        // getSequences();
+    };
+    
+    
+    if (!sequence) {
+        return <p>Loading...</p>
+    }
+    return (
+        <div>
+        {editToggle ? <input
+            type='text'
+            value={sequence.sequenceName}
+            onChange={handleChange}
+            /> : <h2>{sequence.sequenceName}</h2>}
+            <button onClick={handleEditClick}>Edit</button>
+            {sequence.sequencePoses.map((pose) => {
+                return (
+                    <div key={pose._id}>
+                    <p>{pose.englishName}</p>
+                    
+                    <img
+                    src={pose.image}
+                    alt={pose.englishName}
+                    className='image-sequence'
+                    />
+                    {editToggle ? (
+                        <button onClick={() => deleteClick(pose)}>x</button>
+                        ) : (
+                            ''
+                            )}
+                            </div>
+                            );
+                        })}
+                        </div>
+                        );
+                    };
+                    
+                    export default SequenceDetails;
